2193e3c636f07f505ac493ea3401bcccc1ffcee8
MAKCKACHKA <doubleakop@gmail.com>
2023-08-26 01:44:38 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 
-// const SharedLayout = lazy(() => import('./SharedLayout'));
 import SharedLayout from './SharedLayout';
 const Trending = lazy(() => import('../pages/Trending'));
-// import { Trending } from 'pages/Trending';
 const SearchMovie = lazy(() => import('../pages/Searchmovies'));
-// import { SearchMovie } from 'pages/Searchmovies';
 const Details = lazy(() => import('../pages/Moviedetails'));
-// import { Details } from 'pages/Moviedetails';
 const Credits = lazy(() => import('../pages/Moviecredits'));
-// import { Credits } from 'pages/Moviecredits';
 const Reviews = lazy(() => import('../pages/Moviereviews '));
-// import { Reviews } from 'pages/Moviereviews ';
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Trending />} />
-          <Route path="movies" element={<SearchMovie />} />
-          <Route path="movies/:id" element={<Details />}>
-            <Route path="credits" element={<Credits />} />
-            <Route path="reviews" element={<Reviews />} />
+        <Route exact path="/" element={<SharedLayout />}>
+          <Route exact index element={<Trending />} />
+          <Route exact path="movies" element={<SearchMovie />} />
+          <Route exact path="movies/:id" element={<Details />}>
+            <Route exact path="credits" element={<Credits />} />
+            <Route exact path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
